d58c3850b09aad07950f3c8264b56fb4bb27c8b0
Abdulaziz Umar Jada (Adzumr) <adzumrjada@gmail.com>
2022-03-14 20:20:50 +0100

    Setup

---
+++ lib/screens/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ToDoScreen extends StatefulWidget {
+  const ToDoScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ToDoScreen> createState() => _ToDoScreenState();
+}
+
+class _ToDoScreenState extends State<ToDoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: const Expanded(
+            child: Center(
+              child: Text("To Do App"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
