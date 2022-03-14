cbc88f92966b6a8dad099006890ac26d8487a867
Abdulaziz Umar Jada (Adzumr) <adzumrjada@gmail.com>
2022-03-14 20:42:08 +0100

    Updated Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Debt Monitor',
+      debugShowCheckedModeBanner: false,
+      title: 'To Do App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,17 +7,55 @@ class ToDoScreen extends StatefulWidget {
   State<ToDoScreen> createState() => _ToDoScreenState();
 }
 
+bool checkBoxValue = false;
+
 class _ToDoScreenState extends State<ToDoScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
           padding: const EdgeInsets.all(20),
-          child: const Expanded(
-            child: Center(
-              child: Text("To Do App"),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    "Hello!",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "Let's plan your day",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                  child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: const Text("Workout"),
+                    subtitle: const Text("10 Mins"),
+                    trailing: Checkbox(
+                        value: checkBoxValue,
+                        onChanged: (value) {
+                          setState(() {
+                            checkBoxValue = value!;
+                          });
+                        }),
+                  );
+                },
+              )),
+            ],
           ),
         ),
       ),
